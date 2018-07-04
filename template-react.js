c6149b1ff5cf9f31f6a72f854dc8d07387d66d13
Gabriel Santos <gabriielbs@gmail.com>
2018-07-03 23:32:20 -0300

    added pureComponent template

--- template-react.js
+++ template-react.js
@@ -1,10 +1,23 @@
 module.exports = (className, option = 'class') => {
   const addingCss = `import './${className.toLowerCase()}.css'`
+
   const templateClass =
 `import React, { Component } from 'react'
 ${addingCss}
 
-export default class ${className} {
+export default class ${className} extends Component {
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}`
+
+  const templatePure =
+`import React, { PureComponent } from 'react'
+${addingCss}
+
+export default class ${className} extends PureComponent {
   render() {
     return(
       <div></div>
@@ -27,7 +40,11 @@ switch (option) {
   case 'class':
     return templateClass
   case 'function':
+  case 'func':
     return templateFunction
+  case 'pureComponent':
+  case 'pure':
+    return templatePure
   default:
     return templateClass
   }
