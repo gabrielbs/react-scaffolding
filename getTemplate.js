874cf0994fc3a15635b5b92f20009debf0101c38
Gabriel Santos <gabriielbs@gmail.com>
2018-09-05 22:15:37 -0300

    moving templates to a file

---
+++ getTemplate.js
@@ -0,0 +1,44 @@
+module.exports = (options) => {
+  const addingCss = `import './${options.className.toLowerCase()}.css'`
+
+  switch (options.type) {
+    case 'class':
+      return (
+`import React, { Component } from 'react'
+${addingCss}
+
+export default class ${options.className} extends Component {
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}`
+      )
+    case 'pure':
+      return (
+`import React, { PureComponent } from 'react'
+${addingCss}
+
+export default class ${options.className} extends PureComponent {
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}`
+      )
+    case 'function':
+      return (
+`import React from 'react'
+${addingCss}
+
+const ${options.className} = (props) => (
+  <div></div>
+)
+
+export default ${options.className}
+`
+      )
+  }
+}
--- template-react.js
+++ template-react.js
@@ -1,51 +1,23 @@
+const getTemplate = require('./getTemplate')
+
 module.exports = (className, option = 'class') => {
   const addingCss = `import './${className.toLowerCase()}.css'`
 
-  const templateClass =
-`import React, { Component } from 'react'
-${addingCss}
+  const templateClass = getTemplate({ className, type: 'class' })
+  const templatePure = getTemplate({ className, type: 'pure' })
+  const templateFunction = getTemplate({ className, type: 'function' })
 
-export default class ${className} extends Component {
-  render() {
-    return(
-      <div></div>
-    )
-  }
-}`
 
-  const templatePure =
-`import React, { PureComponent } from 'react'
-${addingCss}
-
-export default class ${className} extends PureComponent {
-  render() {
-    return(
-      <div></div>
-    )
-  }
-}`
-
-  const templateFunction =
-`import React from 'react'
-${addingCss}
-
-const ${className} = (props) => (
-  <div></div>
-)
-
-export default ${className}
-`
-
-switch (option) {
-  case 'class':
-    return templateClass
-  case 'function':
-  case 'func':
-    return templateFunction
-  case 'pureComponent':
-  case 'pure':
-    return templatePure
-  default:
-    return templateClass
+  switch (option) {
+    case 'class':
+      return templateClass
+    case 'function':
+    case 'func':
+      return templateFunction
+    case 'pureComponent':
+    case 'pure':
+      return templatePure
+    default:
+      return templateClass
   }
 }
